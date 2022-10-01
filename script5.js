a5539bad8271d85a3be4a5dc252af6ea7b3b7317
anishnitjsr <101205000+anishnitjsr@users.noreply.github.com>
2022-10-01 10:29:02 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,3 @@
+var tweet = prompt("compose your tweet" );
+var tweetcount = tweet.length ;
+alert("you have written " + tweetcount + "charcters , you have" + (140 - tweetcount) + "charcters remaining ");
